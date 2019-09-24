88824f9d890155730c30da874c174db12e69ea1c
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-24 23:10:14 +0000

    Add Feature : Adding function is_num

--- monty.h
+++ monty.h
@@ -39,7 +39,9 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
+int is_num(char *token);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
 
 
 #endif
---
+++ op_utilities.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+int is_num(char *token)
+{
+	int i = 0, res = 1;
+
+	while (token[i] != '\0')
+	{
+		if (token[i] >= 48 && token[i] <= 57)
+			continue;
+		else
+			return (res = 0);
+		i++;
+	}
+	return (res);
+}
+
