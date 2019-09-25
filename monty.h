25c6addae66b8ecaf3bf4d20ee23cd5ab52b17b5
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-25 19:43:01 +0000

    Add Feature : Add pop

--- monty.h
+++ monty.h
@@ -45,6 +45,7 @@ typedef struct instruction_s
 int value;
 int is_num(char *token);
 void _push(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *head);
---
+++ op_monty2.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ *  - adds a new item to the top of a stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line counter
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *aux;
+
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	aux = *stack;
+	if (!aux->next)
+	{
+		*stack = NULL;
+		free(aux);
+	}
+	else
+	{
+	*stack = aux->next;
+	aux->next->prev = NULL;
+	free(aux);
+	}
+}
+
