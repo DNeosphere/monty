ac11cb662d19ad0346e9ab5868e9a1fba637d042
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-25 16:23:00 +0000

    Feature: usage of global variables, declared an defined in header

--- monty.h
+++ monty.h
@@ -22,9 +22,9 @@ extern int value;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -37,12 +37,12 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* Fucntions */
-
+int value;
 int is_num(char *token);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
